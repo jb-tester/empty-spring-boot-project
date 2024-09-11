5270e9514526f7428b48e9cad3f05effbc0ae5aa
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-09-11 13:32:06 +0200

    Initial commit

---
+++ src/main/java/com/mytests/spring/emptyspringbootproject/EmptySpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mytests.spring.emptyspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptySpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmptySpringBootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mytests/spring/emptyspringbootproject/EmptySpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytests.spring.emptyspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptySpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
